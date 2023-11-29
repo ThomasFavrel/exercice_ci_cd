02f42e10f9a706b8d91f5a42fad3d22b3ef633a0
FAVREL Thomas <thomas.favrel@ext.biomerieux.com>
2023-11-29 15:58:01 +0100

    add function

--- run.py
+++ run.py
@@ -7,7 +7,17 @@ def main():
     """
     Main
     """
-    pass
+    import_data()
+
+    
+def import_data():
+    """
+    Import csv file as a dataframe
+    Output: data [pd.DataFrame]
+    """
+    data = pd.read_csv("data/iris.csv")
+    print(data.shape)
+    return data
 
 
 if __name__ == '__main__':
